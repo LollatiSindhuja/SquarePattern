0b1bc150128a470fc2896ce9c0d8fd32c1500fa5
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-28 21:05:58 +0530

    Add files via upload

---
+++ SquarePattern2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Pattern2 {
+    public static void main(String[] args) {
+        int numlines=5;
+        for(int i=1;i<=numlines;i++){
+            if(i%2!=0){
+                System.out.println("* # * # *");
+            }else{
+                System.out.println("# # # # #");
+            }
+        }
+
+    }
+    
+}
